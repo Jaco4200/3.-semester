b77369a5c9c86a681ca155010f7041fd3155b57e
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-11-08 00:27:59 +0100

    create

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import  express  from "express";
+import mysql from "mysql"
+const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"menu"
+})
+
+app.use(express.json())
+app.get("/", (req,res)=>{
+    res.json("hello this is backend!")
+})
+
+app.get("/menu", (req,res)=>{
+    const q = "SELECT * FROM vare"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+app.post("/menu", (req,res)=>{
+    const q = "INSERT INTO menu.vare VALUES (?)"
+    const values = [
+        req.body.id,
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+];
+
+    db.query(q,[values], (err,data)=>{
+        if (err) return res.json(err);
+        return res.json("book");
+    });
+});
+app.listen(8800, ()=>{
+    console.log("loggessd")
+})
