b82eb5d401862314bab500e0186c190dc8e504a6
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-11-08 11:07:04 +0100

    added pages + scss

--- eksamen/src/App.js
+++ eksamen/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Profil from './pages/profil';
 import Menu from './pages/menu';
 import Home from './pages/home';
--- eksamen/src/pages/menu.jsx
+++ eksamen/src/pages/menu.jsx
@@ -1,8 +1,33 @@
 import React from "react";
-
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import '../scss/style.scss'
 const Menu = () => {
+
+const [vare,setVare] = useState ([])
+useEffect(()=>{
+    const fetchAllMenu = async ()=>{
+        try{
+            const res = await axios.get("http://localhost:8800/menu")
+            console.log(res);
+            setVare(res.data); 
+        }catch(err){
+            console.log(err);
+        }
+    }
+    fetchAllMenu()
+}, [])
+
     return (
-        <div>Menu</div>
+        <div className="vare">
+            {vare.map(vare=>(
+                <div className="varer" key={vare.id}>
+                    {vare.cover && <img src={vare.cover} alt="" />}
+                    <h1>{vare.title}</h1>
+                    <p>{vare.desc}</p>
+            </div>))}
+        </div>
     )
 }
 
